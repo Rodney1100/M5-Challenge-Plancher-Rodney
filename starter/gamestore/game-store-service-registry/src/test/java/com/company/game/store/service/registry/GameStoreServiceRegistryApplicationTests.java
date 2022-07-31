ec5e25f633a9ed9d79332bdefb3efb8091606700
rodney1100 <rodney.plancher@gmail.com>
2022-07-30 21:39:00 -0400

    added Eureka

---
+++ starter/gamestore/game-store-service-registry/src/test/java/com/company/game/store/service/registry/GameStoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.game.store.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
